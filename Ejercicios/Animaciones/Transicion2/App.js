d8be75ca4a821f466acee37d7a0fd44022064b5a
memo <memo0p2@hotmail.com>
2021-07-29 18:53:44 -0500

    transiciones

---
+++ Ejercicios/Animaciones/Transicion2/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './Animaciones.css'
+
+const Header = ({ show }) => {
+
+  const clases = show
+    ? 'header header-active'
+    : 'header'
+
+  return (
+    <header className={clases}>
+      <h1>
+        Transiciones CSS en linea
+        <span role='img' aria-label='fire'>
+          🔥
+        </span>
+      </h1>
+    </header>
+  )
+}
+
+const App = () => {
+  const [ active, setActive ] = useState(false)
+
+  const toggle = () => setActive(!active)
+
+  return (
+    <div>
+      <button onClick={toggle}>
+        { active ? 'Desactivar' : 'Activar' }
+      </button>
+      <Header show={active} />
+    </div>
+  )
+}
+
+export default App
